2d72195edcad7deae5affc2f5d318fcb3d3a7fec
tecnologiaanato <tecnologia@anato.org>
2024-12-05 11:41:30 -0500

    ERRORES A CORREGIR

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'anato' );
+define( 'DB_NAME', 'anatoPRUEBA' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
